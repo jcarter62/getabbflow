38e1a1f4b3f949b1a6815a6cab6822b178fdbf7c
jcarter <jcarter62@gmail.com>
2019-10-11 12:42:08 -0700

    multiprocessing changes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from multiprocessing import Pool, Manager
 from getabbflow import GetAbbFlow
 
 sites = [
@@ -5,14 +6,57 @@ sites = [
     {"address": '192.168.30.130', "site": '01RA'},
 
     {"address": '13l.recorder.wwd.local', "site": '13L'},
-    {"address": '12l.recorder.wwd.local', "site": '12L'}
+    {"address": '12l.recorder.wwd.local', "site": '12L'},
+
+    {"address": '11l.recorder.wwd.local', "site": '11L'},
+    {"address": '10l.recorder.wwd.local', "site": '10L'},
+    {"address": '14l.recorder.wwd.local', "site": '14L'},
+    {"address": '15l.recorder.wwd.local', "site": '15L'},
+    {"address": '16l.recorder.wwd.local', "site": '16L'},
 ]
 
 results = {}
 
 
-for site in sites:
-    results[site['site']] = GetAbbFlow(address=site['address'], site=site['site'])
+def my_process(inp_param):
+    address = inp_param['address']
+    site = inp_param['site']
+    print('Start Address = %s' % address)
+    result = GetAbbFlow(address=address, site=site)
+    print('finish address = %s' % address)
+    data = result.data
+    inp_param['q'].put(data)
+    return
+
+
+manager = Manager()
+output_queue = manager.Queue()
+params = list()
+for s in sites:
+    p = {'address': s['address'], 'site': s['site'], 'q': output_queue}
+    params.append(p)
+
+print(params)
+
+# pool = Pool(processes=5)
+pool = Pool()
+print('x')
+pool.map(my_process, params)
+print('y')
+# with Pool() as pool:
+#
+#     pool.map(my_process, params)
+#
+print('Ending program')
+while not output_queue.empty():
+    item = output_queue.get()
+    for row in item:
+        print(row)
+
+print('******** app finished *******')
+
+# for site in sites:
+#     results[site['site']] = GetAbbFlow(address=site['address'], site=site['site'])
 
 # url = 'http://13l.recorder.wwd.local/isapiext.dll/?100000101'
 # address = '13l.recorder.wwd.local'
@@ -20,13 +64,13 @@ for site in sites:
 # data = abb.data
 
 
-def print_results(data):
-    for r in data:
-        _s_ = data[r].timestamp_local + ' - ' + data[r].site
-        print(_s_)
-        for row in data[r].data:
-            _s_ = f'%s %10.3f' % (row['tag'], row['value'])
-            print(_s_)
-
-
-print_results(results)
+# def print_results(data):
+#     for r in data:
+#         _s_ = data[r].timestamp_local + ' - ' + data[r].site
+#         print(_s_)
+#         for row in data[r].data:
+#             _s_ = f'%s %10.3f' % (row['tag'], row['value'])
+#             print(_s_)
+#
+#
+# print_results(results)
--- app2.py
+++ app2.py
@@ -1,13 +1,23 @@
-from multiprocessing import Pool
+from multiprocessing import Pool, Manager
 import time
 import random
 
+print('Starting program')
+datalist = list()
 
-def my_process(num):
+
+def my_process(inp_param):
+    print(inp_param)
+    num = inp_param['num']
     seconds = randomSeconds()
-    print('Start process %d for %d seconds' % (num, seconds))
+    text = 'this is a text field for id: %d' % num
+    text = text + ', and %d seconds' % seconds
+    print(text)
+    data = {'seconds': seconds, 'text': text}
     time.sleep(seconds)
     print('End process %d' % num)
+    inp_param['q'].put(data)
+    #    output_queue.put(data)
     return
 
 
@@ -15,9 +25,21 @@ def randomSeconds():
     return random.randint(2, 5)
 
 
-print('Starting program')
-with Pool(5) as p:
-    p.map(my_process, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+params = list()
 
-# time.sleep(10)
-print('Ending program')
+with Pool(processes=5) as pool:
+    manager = Manager()
+    output_queue = manager.Queue()
+    for i in items:
+        param = {'num': i, 'site': 'site #%d' % i, 'q': output_queue}
+        params.append(param)
+
+    pool.map(my_process, params)
+
+    print('Ending program')
+    while not output_queue.empty():
+        item = output_queue.get()
+        print(item)
+
+print('******** app finished *******')
