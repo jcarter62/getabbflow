7e749d565f02dd392c379785402b8682c05b54ed
jcarter <jcarter62@gmail.com>
2019-10-16 17:14:43 -0700

    initial website

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+import os, json
 from abbdata import AbbData
 
 app = Flask(__name__)
@@ -8,4 +9,18 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    site_names = load_sites_from_file()
+    return render_template('home.html', context={'sites': site_names})
+
+
+def load_sites_from_file():
+    filename = os.path.join(os.path.abspath('.'), 'data.json')
+    with open(filename, 'r') as f:
+        data = json.load(f)
+
+    result = []
+    for s in data['sites']:
+        if s['abb']['urlname'] > '':
+            result.append(s['name'])
+
+    return result
